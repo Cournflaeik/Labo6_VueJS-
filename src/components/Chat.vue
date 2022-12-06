b45152511d7e0355bd3f4393862eb9c35b8a5688
Cournflaeik <elien.degeetere@gmail.com>
2022-12-06 23:16:37 +0100

    feat: add addcomment function

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,16 +1,35 @@
 <script setup>
+let comments = reactive({ comments: []})
+let comment = ref('')
 
+const addComment = () => {
+  comments.comments.push({
+    username: 'Elien',
+    text: comment.value
+  })
+  fetch("https://lab5-p379.onrender.com/api/v1/messages/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: "Elien",
+      text: comment.value,
+    }),
+  })
+  comment.value = ''
+}
 </script>
 
 <template>
   <div class="chat">
     <h2>Comments</h2>
-    <div class="comments">
+    <div v-for="comment in comments.comments" :key="comment.id" class="comments">
       <h3 class="comment__username">
-
+        {{ comment.user }}
       </h3>
       <p class="comment__text">
-
+        {{ comment.text }}
       </p>
     </div>
     <div class="comments__add">
