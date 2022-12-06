1724f7311ba9747b886bded3ebfdfaa5f6892ed9
Cournflaeik <elien.degeetere@gmail.com>
2022-12-07 00:56:08 +0100

    fix: reverse comments

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -8,13 +8,14 @@ onMounted(() => {
   fetch("https://lab5-p379.onrender.com/api/v1/messages/")
     .then((response) => response.json())
     .then((data) => {
+      data = data.reverse()
       comments.comments = data
     })
 })
 
 const addComment = () => {
   comments.comments.push({
-    username: 'Elien',
+    user: 'Elien',
     text: comment.value
   })
   fetch("https://lab5-p379.onrender.com/api/v1/messages/", {
@@ -23,7 +24,7 @@ const addComment = () => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      username: "Elien",
+      user: "Elien",
       text: comment.value,
     }),
   })
@@ -34,21 +35,84 @@ const addComment = () => {
 <template>
   <div class="chat">
     <h2>Comments</h2>
-    <div v-for="comment in comments.comments" :key="comment.id" class="comments">
-      <h3 class="comment__username">
-        {{ comment.user }}
-      </h3>
-      <p class="comment__text">
-        {{ comment.text }}
-      </p>
-    </div>
     <div class="comments__add">
       <input type="text" v-model="comment" placeholder="Add a comment...">
       <button @click="addComment">Post</button>
+    </div>
+    <div class="comments">
+      <div v-for="comment in comments.comments" :key="comment.id">
+        <h3 class="comment__username">
+          {{ comment.user }}
+        </h3>
+        <p class="comment__text">
+          {{ comment.text }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+  .chat {
+    height: 100%;
+  }
+  .comments {
+    max-height: 70vh;
+    margin-bottom: 1rem;
+    overflow-y: scroll;
+  }
+  .comments__add {
+    display: flex;
+    justify-content: space-between;
+  }
+  .comment__username {
+    font-weight: bold;
+  }
+  button {
+  align-items: center;
+  appearance: none;
+  background-image: radial-gradient(100% 100% at 100% 0, #5adaff 0, #5468ff 100%);
+  border: 0;
+  border-radius: 6px;
+  box-shadow: rgba(45, 35, 66, .4) 0 2px 4px,rgba(45, 35, 66, .3) 0 7px 13px -3px,rgba(58, 65, 111, .5) 0 -3px 0 inset;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  display: inline-flex;
+  font-family: "JetBrains Mono",monospace;
+  height: 48px;
+  justify-content: center;
+  line-height: 1;
+  list-style: none;
+  overflow: hidden;
+  padding-left: 16px;
+  padding-right: 16px;
+  position: relative;
+  text-align: left;
+  text-decoration: none;
+  transition: box-shadow .15s,transform .15s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+  will-change: box-shadow,transform;
+  font-size: 18px;
+  margin-right: 2rem;
+}
+
+button:focus {
+  box-shadow: #3c4fe0 0 0 0 1.5px inset, rgba(45, 35, 66, .4) 0 2px 4px, rgba(45, 35, 66, .3) 0 7px 13px -3px, #3c4fe0 0 -3px 0 inset;
+}
+
+button:hover {
+  box-shadow: rgba(45, 35, 66, .4) 0 4px 8px, rgba(45, 35, 66, .3) 0 7px 13px -3px, #3c4fe0 0 -3px 0 inset;
+  transform: translateY(-2px);
+}
+
+button:active {
+  box-shadow: #3c4fe0 0 3px 7px inset;
+  transform: translateY(2px);
+}
+  
 
 </style>
