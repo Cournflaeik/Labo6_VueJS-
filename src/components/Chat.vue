9f994679d49f4ca61f398fadb88325cf7b655870
Cournflaeik <elien.degeetere@gmail.com>
2022-12-06 23:03:35 +0100

    feat: add comment html

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -3,8 +3,20 @@
 </script>
 
 <template>
-  <div>
-    Chat.vue
+  <div class="chat">
+    <h2>Comments</h2>
+    <div class="comments">
+      <h3 class="comment__username">
+
+      </h3>
+      <p class="comment__text">
+
+      </p>
+    </div>
+    <div class="comments__add">
+      <input type="text" v-model="comment" placeholder="Add a comment...">
+      <button @click="addComment">Post</button>
+    </div>
   </div>
 </template>
 
